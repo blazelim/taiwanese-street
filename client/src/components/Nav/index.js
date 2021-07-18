0a178c13cb0f4f6c1ad2a35997aa06303514f28d
ACB <78452262+adambedingfield@users.noreply.github.com>
2021-07-17 20:18:20 -0700

    setup basic nav structure, created style for header

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -1,1 +1,33 @@
-import React from 'react';
+import React from "react";
+import Auth from "../../utils/auth";
+import { Link } from "react-router-dom";
+
+function Nav() {
+
+  function showNavigation() {
+    return(
+        <ul className="nav-list">
+            <li>Menu</li>
+            <li>Order Now</li>
+            <li>Location</li>
+            <li>Our Story</li>
+            <li>User</li>
+            <li>Cart</li>
+        </ul>
+    )
+  }
+
+  return (
+    <header className="header">
+      <h1 className="company">
+        Taiwanese Street
+      </h1>
+
+      <nav className="nav">
+        {showNavigation()}
+      </nav>
+    </header>
+  );
+}
+
+export default Nav;
