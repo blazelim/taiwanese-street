12ce6703700c5d6dad93ef5e01b79bc37487d9bd
Blaze Lim <blazebentleycolim@gmail.com>
2021-07-18 19:41:09 -0700

    added query and mutations type defs

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -42,6 +42,21 @@ const typeDefs = gql`
 
     type Query {
         categories: [Category]
-        products(category: ID, name: String): [Product]
+        products(category: ID, name: String): [Product],
+        product(_id: ID!): Product,
+        user: User
+        order(_id: ID!): Order
+        checkout(products: [ID]!): Checkout
     }
+
+    type Mutation {
+        addUser(firstName: String!, lastName: String!, email: String!, password: String!): Auth
+        addOrder(products: [ID]!): Order
+        updateUser(firstName: String, lastName: String, email: String, password: String): User
+        updateProduct(_id: ID!, quantity: Int!): Product
+        login(email: String!, password: String!): Auth
+      }
+    
 `
+
+module.exports = typeDefs
