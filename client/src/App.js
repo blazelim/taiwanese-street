3df70fd54809d8c8313e6fb21c9dc698f0f94234
ACB <78452262+adambedingfield@users.noreply.github.com>
2021-07-18 17:19:35 -0700

    navbar functional, finished login/signup, setup app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,51 @@
-import { Router } from 'react-router-dom';
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
 import Nav from './components/Nav';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+
+const httpLink = createHttpLink({
+  uri: '/graphql',
+});
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('id_token');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
+
+
 
 function App() {
   return (
-      <Nav />
+    <ApolloProvider client={client}>
+      <Router>
+        <div>
+            <Nav />
+            <Switch>
+              <Route exact path="/login" component={Login} />
+              <Route exact path="/signup" component={Signup} />
+            </Switch>
+        </div>
+      </Router>
+    </ApolloProvider>
   );
 }
 
--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -7,12 +7,13 @@ function Nav() {
   function showNavigation() {
     return(
         <ul className="nav-list">
-            <li>Menu</li>
-            <li>Order Now</li>
-            <li>Location</li>
-            <li>Our Story</li>
-            <li>User</li>
-            <li>Cart</li>
+            <li><Link className="link" to="/menu">Menu</Link></li>
+            <li><Link className="link" to="/order">Order now</Link></li>
+            <li><Link className="link" to="/location">Location</Link></li>
+            <li><Link className="link" to="/about">About</Link></li>
+            <li><Link className="link" to="/login">Login</Link></li>
+            <li><Link className="link" to="/signup">Signup</Link></li>
+            <li><Link className="link cart" to="/cart">&#128722;</Link></li>
         </ul>
     )
   }
@@ -20,9 +21,8 @@ function Nav() {
   return (
     <header className="header">
       <h1 className="company">
-        Taiwanese Street
+          Taiwanese Street
       </h1>
-
       <nav className="nav">
         {showNavigation()}
       </nav>
